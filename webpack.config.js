f0aa42ce01ca0579f1850dff0089839545da02e9
Chris Gosser <rivetkopf@gmail.com>
2018-10-15 00:21:10 -0700

    add dotenv as a dev dependency

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'GENERIC',
+      title: 'doctor',
       template: './src/index.html',
       inject: 'body'
     })
