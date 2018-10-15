2edc5921452da0262e4dffd23e9b80248de428fc
Chris Gosser <rivetkopf@gmail.com>
2018-10-15 07:37:46 -0700

    tried a lot of different things to make successful api call, also renamed api key variable in .env

--- src/backend.js
+++ src/backend.js
@@ -1,32 +1,51 @@
-function doctorSearch(searchTerm , searchType){
+import $ from 'jquery';
+export function doctorSearch(searchTerm , searchType){
   if (searchType == "condition") {
     $.ajax({
-      url: "https://api.betterdoctor.com/2016-03-01/doctors?query=${searchterm}&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=${process.env.API_KEY}",
+      url: "https://api.betterdoctor.com/2016-03-01/doctors?query=" + searchTerm + "&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=" + process.exports.apiKey,
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
+        console.log("success");
         return response
       },
       error: function() {
-        return "error"
+        console.log(`There was an error processing your request. Please try again.`);
       }
     });
   }
   if (searchType == "name") {
     $.ajax({
-      url: "https://api.betterdoctor.com/2016-03-01/doctors?name=${searchterm}&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=${process.env.API_KEY}",
+      url: "https://api.betterdoctor.com/2016-03-01/doctors?name=" + searchTerm + "&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=" + process.exports.apiKey,
       type: 'GET',
       data: {
         format: 'json'
       },
       success: function(response) {
+        console.log("success");
         return response
       },
       error: function() {
-        return "error"
+        console.log(`There was an error processing your request. Please try again.`);
       }
     });
   }
 }
+// if (searchType == "condition") {
+//   $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + searchTerm + "&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=" + process.env.API_KEY).then(function(response) {
+//       return response
+//       }).fail(function(error) {
+//         $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+//       });
+//
+//   };
+// if (searchType == "name") {
+//   $.get("https://api.betterdoctor.com/2016-03-01/doctors?name=" + searchTerm + "&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=" + process.env.API_KEY).then(function(response) {
+//       return response
+//       }).fail(function(error) {
+//         $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+//       });
+// }
+// }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
 import $ from 'jquery';
-import {doctorSearch} from 'backend.js'
+import {doctorSearch} from './backend'
+
+$(document).ready(function() {
+  var searchResult = doctorSearch("feet", "condition")
+  console.log(searchResult);
+})
