0f20d339811609270906b519ee94ef7ab827e05f
Chris Gosser <rivetkopf@gmail.com>
2018-10-15 07:52:17 -0700

    add frontend functionality for input fields

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import $ from 'jquery';
 import {doctorSearch} from './backend'
 
 $(document).ready(function() {
-  var searchResult = doctorSearch("feet", "condition")
-  console.log(searchResult);
+  var searchType = $("#searchType").val()
+  var userInput = $("#userInput").val()
+  var searchResult = doctorSearch(userInput, searchType)
+
+  $("#userInput").append(searchResult.data[1].profile.first_name)
+
 })
