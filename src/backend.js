17487595d599c0b3008c5ed26627dc0db0a0b04d
Chris Gosser <rivetkopf@gmail.com>
2018-10-17 00:24:19 -0700

    refactor api call to no longer use jquery, it now at least works in back end

--- src/backend.js
+++ src/backend.js
@@ -1,51 +1,29 @@
 import $ from 'jquery';
-export function doctorSearch(searchTerm , searchType){
+export default class Doctor{
+doctorSearch(searchTerm , searchType){
   if (searchType == "condition") {
-    $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?query=` + searchTerm + `&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=` + process.env.exports.apiKey,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        console.log("success");
-        return response
-      },
-      error: function() {
-        console.log(`There was an error processing your request. Please try again.`);
+    let request = new XMLHttpRequest();
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${searchTerm}&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
       }
-    });
+    }
+
+
   }
   if (searchType == "name") {
-    $.ajax({
-      url: "https://api.betterdoctor.com/2016-03-01/doctors?name=" + searchTerm + "&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=" + process.env.exports.apiKey,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        console.log("success");
-        return response
-      },
-      error: function() {
-        console.log(`There was an error processing your request. Please try again.`);
+    let request = new XMLHttpRequest();
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${searchTerm}&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
       }
-    });
-  }
+    }
+  request.open("GET", url, true);
+  request.send();
 }
-// if (searchType == "condition") {
-//   $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + searchTerm + "&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=" + process.env.API_KEY).then(function(response) {
-//       return response
-//       }).fail(function(error) {
-//         $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
-//       });
-//
-//   };
-// if (searchType == "name") {
-//   $.get("https://api.betterdoctor.com/2016-03-01/doctors?name=" + searchTerm + "&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=" + process.env.API_KEY).then(function(response) {
-//       return response
-//       }).fail(function(error) {
-//         $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
-//       });
-// }
-// }
+}
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import $ from 'jquery';
-import {doctorSearch} from './backend'
+import {Doctor} from './backend'
 
 $(document).ready(function() {
   var searchType = $("#searchType").val()
   var userInput = $("#userInput").val()
-  var searchResult = doctorSearch(userInput, searchType)
-
-  $("#userInput").append(searchResult.data[1].profile.first_name)
-
+  var
+  var searchResult = Doctor.doctorSearch("steve", "name")
+  console.log(searchResult);
+  $(".outputs").append("<p>" + searchResult.data[1].profile.first_name + "</p>")
 })
