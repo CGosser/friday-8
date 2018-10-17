9f5b7249427530cf029b60d156d2ed98733c359c
Chris Gosser <rivetkopf@gmail.com>
2018-10-17 01:47:56 -0700

    display results of api call as a list of doctor names matching search criteria

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@ import {Doctor} from './backend'
 
 $(document).ready(function() {
   $('#searchInput').click(function() {
-    var searchType = $("#searchType").val()
-    var userInput = $("#userInput").val()
+    $('.outputs').empty()
+    let searchType = $("#searchType").val()
+    let userInput = $("#userInput").val()
     let doctor = new Doctor()
-    let promise = doctor.doctorSearch("feet", "condition")
+    let promise = doctor.doctorSearch(userInput, searchType)
     promise.then(function(response){
       let body = JSON.parse(response);
-      $('.outputs').append("<p>" + body.data[0].profile.first_name + "</p>")
+      if (body.data.length > 0){
+      for (let i = 0; i < body.data.length; i++){
+        $('.outputs').append("<ul><li>" + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "</li>" + "</ul>" )
+
+
+      }
+    } else {
+      $('.outputs').append("<h1>no results for your search</h1>")
+    }
+
     })
   })
 
