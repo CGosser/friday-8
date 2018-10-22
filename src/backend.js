00c919f3a6a5348e320506b19c18e4b63d28183d
Chris Gosser <rivetkopf@gmail.com>
2018-10-22 01:42:58 -0700

    add outputs for the address, making sure it's the correct practice if the doctor has multiple practices

--- src/backend.js
+++ src/backend.js
@@ -2,40 +2,20 @@ import $ from 'jquery';
 
 export class Doctor{
   doctorSearch(searchTerm , searchType){
-    if (searchType == "condition") {
-      return new Promise(function(resolve, reject) {
-        let request = new XMLHttpRequest();
-        const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${searchTerm}&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?${searchType}=${searchTerm}&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
-        request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          }
-          else{
-            reject(Error(request.statusText));
-          }
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
         }
-        request.open("GET", url, true);
-        request.send();
-      });
-    }
-    if (searchType == "name") {
-      return new Promise(function(resolve, reject) {
-
-        let request = new XMLHttpRequest();
-        const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${searchTerm}&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-
-        request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          }
-          else{
-            reject(Error(request.statusText));
-          }
+        else{
+          reject(Error(request.statusText));
         }
-        request.open("GET", url, true);
-        request.send();
-      });
-    }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,38 @@
 import $ from 'jquery';
 import {Doctor} from './backend'
-
+function isAcceptingNewPatients(accepting){ if (accepting){
+  return "Is currently accepting new patients"
+} else {
+  return "Is currently <strong> not </strong> accepting new patients"
+}
+}
 $(document).ready(function() {
   $('#searchInput').click(function() {
+    console.log("clicked");
     $('.outputs').empty()
     let searchType = $("#searchType").val()
     let userInput = $("#userInput").val()
     let doctor = new Doctor()
     let promise = doctor.doctorSearch(userInput, searchType)
     promise.then(function(response){
-      let body = JSON.parse(response);
+      console.log(response);
+      const body = JSON.parse(response);
       if (body.data.length > 0){
         for (let i = 0; i < body.data.length; i++){
-          $('.outputs').append("<ul><li>" + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "</li>" + "</ul>" )
+          const practice = body.data[i].practices.findIndex(practices => practices.location_slug == "or-portland")
+
+
+          $('.outputs').append("<img src='" + body.data[i].profile.image_url + "'>" +
+          "<ul><li>" + body.data[i].profile.first_name + " " + body.data[i].profile.last_name + "</li>" +
+          "<li>" + body.data[i].practices[practice].phones[0].number + "</li>" +
+          "<li>" + isAcceptingNewPatients(body.data[i].practices[practice].accepts_new_patients) + "</li><br>" +
+
+          "<li>" + "<strong>office address:</strong>" + "</li>" +
+          "<li>" + body.data[i].practices[practice].visit_address.street + "</li>" +
+          "<li>" + body.data[i].practices[practice].visit_address.city + "</li>" +
+          "<li>" + body.data[i].practices[practice].visit_address.state + "</li>" +
+          "<li>" + body.data[i].practices[practice].visit_address.zip + "</li>" +
+          "</ul>" )
         }
       } else {
         $('.outputs').append("<h1>no results for your search</h1>")
